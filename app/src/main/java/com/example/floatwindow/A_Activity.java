1ca6344ac75ba58aeb10cce8d793e064b767c23f
楚留香 <1440355743@qq.com>
2020-04-20 22:01:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/floatwindow/A_Activity.java
@@ -0,0 +1,22 @@
+package com.example.floatwindow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class A_Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a);
+        setTitle("A");
+    }
+
+    public void change(View view) {
+        startActivity(new Intent(this, B_Activity.class));
+    }
+}
